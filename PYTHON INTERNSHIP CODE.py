bb0f90dd7f46160c588ae818a15737c66b814520
OMESH6303 <kondetiomeshnaidu@gmail.com>
2024-06-11 19:11:29 +0530

    Add files via upload

---
+++ PYTHON INTERNSHIP CODE.py
@@ -0,0 +1,40 @@
+Python 3.12.0 (tags/v3.12.0:0fb18b0, Oct  2 2023, 13:03:39) [MSC v.1935 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: C:/Users/konde/AppData/Local/Programs/Python/Python312/python intern.py
+Welcome to the Quiz Game!
+
+What is 2+2?
+1. 1
+2. 2
+3. 3
+4. 4
+Your answer (1-4): 4
+Correct!
+
+What is the capital of France?
+1. Paris
+2. London
+3. Berlin
+4. Rome
+Your answer (1-4): 3
+Incorrect. The correct answer is: 1.
+
+What is the powerhouse of the cell?
+1. Mitochondria
+2. Nucleus
+3. Ribosome
+4. Golgi Apparatus
+Your answer (1-4): 2
+Incorrect. The correct answer is: 1.
+
+Your final score is: 1/3
+>>> 
+= RESTART: C:/Users/konde/AppData/Local/Programs/Python/Python312/python intern.py
+Welcome to the Quiz Game!
+
+What is 2+2?
+1. 1
+2. 2
+3. 3
+4. 4
